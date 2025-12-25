572cd89b1e1b94635c679dcb9578290f9aad1ad1
Alexandre <alexandre.regnier59@live.fr>
2025-12-25 12:12:22 +0100

    Atomic commit : Finalisation de la gestion de bdd en python, finalisation du module web S1, apprentissage du binaire et de l'HEX (conversion binaire - hex et inversement, binaire pos to neg et inversement...

--- ANNEE_1_FONDATIONS/R1.05_Bases_de_Donnees/02_Python_Integration/sql_in_py_1/main.py
+++ ANNEE_1_FONDATIONS/R1.05_Bases_de_Donnees/02_Python_Integration/sql_in_py_1/main.py
@@ -1,14 +1,54 @@
 import sqlite3
 
+
 def ajouter_etudiant(nom, prenom):
     with sqlite3.connect("ecole.db") as connection:
-        cur = connection.cursor()
-
+        cur = connection.cursor() 
         cur.execute("INSERT INTO etudiant(nom, prenom) VALUES (?, ?)",(nom, prenom))
         connection.commit() 
+       
+        
 
-           
+def afficher_etudiant():
+    with sqlite3.connect("ecole.db") as connection:
+        cur = connection.cursor()
+        cur.execute("SELECT e.prenom, n.resultat, n.matiere FROM etudiant as e LEFT JOIN note AS n ON e.id = etudiant_id")
+        etudiants = cur.fetchall()
+        for etudiant in etudiants:
+            prenom, note, matiere = etudiant
+            if note is None:
+                print(f"L'étudiant {prenom} n'a pas encore de note")
+            else:
+                print(f"L'étudiant {prenom} a obtenu la note de {note} en {matiere}")
+
+
+def ajouter_note(etudiant_id, matiere, resultat):
+    with sqlite3.connect("ecole.db") as connection:
+        cur = connection.cursor()
+        #NE PAS OUBLIER CETTE LIGNE POUR LA LIAISON PK FK 
+        cur.execute("PRAGMA foreign_keys = ON")
+        
+        try:
+            cur.execute("INSERT INTO note (matiere, resultat, etudiant_id) VALUES (?, ?, ?)", 
+                        (matiere, resultat, etudiant_id))
+            connection.commit()
+            print("Note ajoutée avec succès.")
+            
+        except sqlite3.IntegrityError:
+            print(f"Erreur : L'étudiant ID {etudiant_id} n'existe pas !")
+
+def delete_etudiant():
+    with sqlite3.connect("ecole.db") as connect:
+        cur = connect.cursor()
+
+        cur.execute("""
+            DELETE FROM 
+                    etudiant
+                WHERE
+                    prenom = 'test'   
+        """)
 
 if __name__ == "__main__":
-    ajouter_etudiant("Alex", "test")
-       
+    afficher_etudiant()
+
+    
